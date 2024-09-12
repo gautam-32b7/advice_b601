271cb34863064bdf0b0314f9f288ee7ce6fa546d
Gautam Sarkar <gautam_32b7@hotmail.com>
2024-09-12 07:16:52 +0530

    add views

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import { __express as ejsEngine } from 'ejs';
+
 const app = express();
 const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.engine('ejs', ejsEngine);
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.render(__dirname + '/views/index.ejs');
 });
 
 app.listen(port, () => {
