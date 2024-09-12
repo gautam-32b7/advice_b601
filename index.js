a9fb1f6f33d8852196809476156f1c9ecbb74329
Gautam Sarkar <gautam_32b7@hotmail.com>
2024-09-12 07:22:33 +0530

    add list of advices

--- index.js
+++ index.js
@@ -13,8 +13,30 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const advices = {
+  0: 'Focus on progress, not perfection.',
+  1: 'Consistency beats intensity in the long run.',
+  2: "Learn to say 'no' to protect your time and energy.",
+  3: 'Don’t be afraid to fail; failure is a stepping stone to success.',
+  4: 'Take breaks to recharge your creativity and productivity.',
+  5: 'Always keep learning; knowledge is the best investment.',
+  6: 'Surround yourself with people who inspire and challenge you.',
+  7: 'Manage your time, or it will manage you.',
+  8: 'Listen more than you speak; you’ll learn more that way.',
+  9: 'Small actions daily lead to big changes over time.',
+};
+
 app.get('/', (req, res) => {
   res.render(__dirname + '/views/index.ejs');
+});
+
+app.post('/submit', (req, res) => {
+  const random = Math.floor(Math.random() * 10);
+  const advice = advices[random];
+  const context = {
+    advice,
+  };
+  res.render(__dirname + '/views/index.ejs', context);
 });
 
 app.listen(port, () => {
